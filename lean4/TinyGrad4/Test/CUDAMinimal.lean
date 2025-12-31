a631f199119ab5de7674bcb6f5874da3182da6b1
Alok Singh <alokbeniwal@gmail.com>
2025-12-31 11:14:17 -0800

    test: add runAllBenchmarks call to minimal test

--- lean4/TinyGrad4/Test/CUDAMinimal.lean
+++ lean4/TinyGrad4/Test/CUDAMinimal.lean
@@ -1,4 +1,5 @@
 import TinyGrad4.Benchmark.CudaBenchmark
+import TinyGrad4.Benchmark.Framework
 
 /-!
 # Minimal CUDA Benchmark Test
@@ -6,7 +7,14 @@ import TinyGrad4.Benchmark.CudaBenchmark
 Just imports CudaBenchmark to test if the issue is in module initialization.
 -/
 
+open TinyGrad4.Benchmark
+open TinyGrad4.Benchmark.Cuda
+
 def main : IO UInt32 := do
   IO.println "Starting minimal test..."
-  IO.println "If you see this, module loading worked!"
+  IO.println "Module loading worked!"
+  IO.println ""
+  IO.println "Calling runAllBenchmarks..."
+  let _results ← runAllBenchmarks
+  IO.println "Done!"
   return 0

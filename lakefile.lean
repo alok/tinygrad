23095caeff5ab2a893e5ea295dcbc1b8d4bd2785
Alok Singh <alokbeniwal@gmail.com>
2026-01-04 13:21:27 -0800

    lake: move mathlib dep to top-level

--- lakefile.lean
+++ lakefile.lean
@@ -15,6 +15,7 @@ package TinyGrad4 where
 
 require batteries from git "https://github.com/leanprover-community/batteries" @ "main"
 require Cli from git "https://github.com/leanprover/lean4-cli" @ "main"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.27.0-rc1"
 require LeanBench from git "https://github.com/alok/leanbench" @ "ae0820b"
 
 def cFlags : Array String :=
